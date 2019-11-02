e4b60718c53f39c2a57853795a069e070a2831af
Darek-Mac <darek.baranczuk@gmail.com>
2019-11-02 21:54:03 +0100

    Inital commit

---
+++ app/src/main/java/com/passwordvault/auto/fill/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.passwordvault.auto.fill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
